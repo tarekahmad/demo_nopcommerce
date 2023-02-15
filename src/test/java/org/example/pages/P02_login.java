bc769ff9746c9473d30743b698f9d6659740ffa3
tarekahmad <tarek.ahmed9710@gmail.com>
2023-02-15 22:30:39 +0200

    first commit

--- src/test/java/org/example/pages/P02_login.java
+++ src/test/java/org/example/pages/P02_login.java
@@ -1,11 +1,28 @@
 package org.example.pages;
 
 import org.example.stepDefs.Hooks;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
 
 public class P02_login {
     public P02_login(){
         PageFactory.initElements(Hooks.driver,this);
     }
+
+    @FindBy(className=("ico-login"))
+    public WebElement loginlink;
+    @FindBy(id=("Email"))
+    public WebElement Email;
+    @FindBy(id=("Password"))
+    public WebElement Password;
+    @FindBy(className=("button-1 login-button"))
+    public WebElement loginbutton;
+    @FindBy(className=("result"))
+    public WebElement succesmessage;
+
+
+
+
 
 }
--- src/test/java/org/example/stepDefs/D02_loginStepDef.java
+++ src/test/java/org/example/stepDefs/D02_loginStepDef.java
@@ -12,10 +12,12 @@ import org.openqa.selenium.support.Color;
 import org.openqa.selenium.support.ui.Select;
 import org.testng.asserts.SoftAssert;
 public class D02_loginStepDef {
-    P02_login register=new P02_login();
+    P02_login Login=new P02_login();
 
     @Given("user go to login page")
     public void user_go_to_login_page() {
+        Login.loginlink.click();
+
     }
 
     @When("user login with {string} {string} and {string}")
